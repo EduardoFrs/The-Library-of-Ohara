915957dc9ad7702aef60c1b69e0ef2d93d4a3e6f
EduardoFrs <fernandeseduardo00@gmail.com>
2024-08-29 15:17:42 +0200

    Implement login and logout

--- the_library_of_ohara/myapp/views.py
+++ the_library_of_ohara/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
@@ -28,5 +28,11 @@ def user_login(request):
         if user is not None:
             login(request, user)
             return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
 
--- the_library_of_ohara/the_library_of_ohara/urls.py
+++ the_library_of_ohara/the_library_of_ohara/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
